ca1c3777a8ab8b0b0003f664051658b21b740904
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2024-10-09 10:55:02 +0100

    host

--- static/fetch.js
+++ static/fetch.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/hello')
+fetch('window.location.host')
     .then(response => console.log(response.status) || response) // output the status and return response
     .then(response => response.text()) // send response body to next then chain
     .then(body => {
