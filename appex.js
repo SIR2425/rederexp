9d7b2bc6236675b1ae723c3e8984fb170c9f7d7f
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2024-10-09 10:45:15 +0100

    ports

--- appex.js
+++ appex.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.use(express.static('static'));
 app.get('/hello', (req,res) => res.send("Hello Express"));
 app.listen(port,() => console.log("Server with expressJS is running"));
