8445a048af7c4cd2ebd2afa372dffcd83fa692a3
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2024-10-09 10:58:33 +0100

    bug host

--- static/fetch.js
+++ static/fetch.js
@@ -1,4 +1,4 @@
-fetch('window.location.host')
+fetch(window.location.host)
     .then(response => console.log(response.status) || response) // output the status and return response
     .then(response => response.text()) // send response body to next then chain
     .then(body => {
